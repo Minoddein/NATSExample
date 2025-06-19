be71bb263594ff435814a519cc798896da6ce2de
MinoddeinEz <kamazitun.jaykehd@gmail.com>
2025-06-19 15:46:55 +0300

    feat: cart and cart item has been implemented

--- ShopService/src/Domain/CartManagement/Aggregate/Cart.cs
+++ ShopService/src/Domain/CartManagement/Aggregate/Cart.cs
@@ -1,6 +1,58 @@
-﻿namespace Domain.CartManagement.Aggregate;
+﻿using Domain.CartManagement.Entities;
+using Domain.Shared;
 
-public class Cart
+namespace Domain.CartManagement.Aggregate;
+
+public sealed class Cart: DomainEntity
 {
-    
+    private readonly List<CartItem> _items = [];
+
+    public Cart(Guid id, Guid userId)
+    {
+        if (Guid.Empty.Equals(id))
+        {
+            throw new ArgumentNullException(nameof(id));
+        }
+
+        if (Guid.Empty.Equals(userId))
+        {
+            throw new ArgumentNullException(nameof(userId));
+        }
+
+        Id = id;
+        UserId = userId;
+    }
+
+    //Мок, рандомный гуид, без привязки к AuthService и схемам Identity
+    public Guid UserId { get; private set; }
+    public IReadOnlyList<CartItem> Items => _items;
+    public int TotalCount => _items.Count;
+
+    public void AddItem(CartItem item)
+    {
+        var isExist = _items.FirstOrDefault(i => i.Id == item.Id);
+        if (isExist is not null) 
+        {
+            isExist.Increase();
+            return;
+        }
+        
+        _items.Add(item);
+
+        var @event = new ItemAddedEvent();
+        
+        AddEvent(@event);
+    }
+
+    public void RemoveItem(CartItem item)
+    {
+        
+    }
+
+    public void ClearAllTheItems(CartItem item)
+    {
+        
+    }
 }
+
+public record ItemAddedEvent(): IDomainEvent;
--- ShopService/src/Domain/CartManagement/Entities/CartItem.cs
+++ ShopService/src/Domain/CartManagement/Entities/CartItem.cs
@@ -1,6 +1,55 @@
-﻿namespace Domain.CartManagement.Entities;
+﻿using CSharpFunctionalExtensions;
+using Domain.ShopManagement.Entities;
 
-public class CartItem
+namespace Domain.CartManagement.Entities;
+
+public sealed class CartItem : Entity<Guid>
 {
+    public CartItem(Guid id, Guid productId, int quantity, DateTime dateCreated)
+    {
+        #region constraints
+        if (Guid.Empty == productId)
+        {
+            throw new ArgumentException("ProductId cannot be empty");
+        }
+
+        if (Guid.Empty == id)
+        {
+            throw new ArgumentException("Id cannot be empty");
+        }
+
+        if (quantity <= 0)
+        {
+            throw new ArgumentException("Quantity cannot be zero or negative");
+        }
+
+        if (dateCreated > DateTime.Now)
+        {
+            throw new ArgumentException("Date cannot be in the future");
+        }
+        #endregion
+        
+        Id = id;
+        ProductId = productId;
+        Quantity = quantity;
+        DateCreated = dateCreated;
+    }
+    
+    public Guid ProductId { get; private set; }
+    public int Quantity { get; private set; }
+    public DateTime DateCreated { get; private set; }
+
+    public void Reduce()
+    {
+        if (Quantity > 0)
+        {
+            Quantity--;
+        }
+    }
+
+    public void Increase()
+    {
+        Quantity++;
+    }
     
 }
